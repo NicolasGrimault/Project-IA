7924dd47090ce08ccf6f0270b20ae2ef23a4d5a6
Nicolas Grimault <nicolas.g21@live.fr>
2019-04-15 15:50:12 +0200

    Concat data

--- python.py
+++ python.py
@@ -3,4 +3,7 @@
 import pandas as pd
 
 train = pd.read_csv('Data/TrainingSetValues.csv', header = 0)
-test  = pd.read_csv('Data/TrainingSetLabels.csv' , header = 0)
+
+#%% 
+#Display data 
+train.info()
