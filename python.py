99bbaceaf46c3007ca77d99769324573d4542262
Nicolas Grimault <nicolas.g21@live.fr>
2019-04-17 15:36:05 +0200

    Remove unvalid mapping

--- python.py
+++ python.py
@@ -13,7 +13,6 @@ train['status_group'] = train['status_group'].map({'functional': 0, 'functional 
 train['source_class'] = train['source_class'].map({'groundwater': 0, 'surface': 1, 'unknown': 2}).astype(int)
 train['extraction_type'] = train['extraction_type'].map({ 'gravity': 0, 'submersible':1,'swn 80':2,'nira/tanira':3,'india mark ii':4,'other':5,'ksb':6,'mono':7,'windmill':8,'afridev':9,'other - rope pump':10,'india mark iii':11,'other - swn 81':12,'other - play pump':13,'cemo':14,'climax':15,'walimi':16,'other - mkulima/shinyanga':17}).astype(int)
 train['water_quality'] = train['water_quality'].map({'coloured': 0, 'fluoride': 1, 'fluoride abandoned': 2, 'milky': 3, 'salty': 4, 'salty abandoned': 5, 'soft': 6, 'unknown': 7}).astype(int)
-train['quality_group'] = train['quality_group'].map({'good': 0, 'milky': 1, 'salty': 2, 'unknown': 3}).astype(int)
 train['quantity'] = train['quantity'].map({'dry': 0, 'enough': 1, 'insufficient': 2, 'seasonal': 3, 'unknown': 4}).astype(int)
 train['quantity_group'] = train['quantity_group'].map({'dry': 0, 'enough': 1, 'insufficient': 2, 'seasonal': 3, 'unknown': 4}).astype(int)
 train['source'] = train['source'].map({'dam': 0, 'hand dtw': 1, 'lake': 2, 'machine dbh': 3, 'other': 4, 'rainwater harvesting': 5, 'river': 6, 'shallow well': 7, 'spring': 8, 'unknown': 9}).astype(int)
