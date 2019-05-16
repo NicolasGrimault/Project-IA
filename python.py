7eab5d9141529fc46c0278cf303c9005325b90cd
Nicolas Grimault <nicolas.g21@live.fr>
2019-05-16 11:25:02 +0200

    Refacto

--- python.py
+++ python.py
@@ -10,48 +10,39 @@ from sklearn.metrics import accuracy_score
 lab_enc = preprocessing.LabelEncoder()
 
 train = pd.read_csv('Data/TrainingSetValues.csv', header=0, sep = ",")
-#train.info()
 
 # %%
-# Display data
-train.info()
+# Process specific data
 
-# %%
-train['funder'] = train['funder'].fillna('NAN')
-train.loc[train['funder'] == '0', 'funder'] = 'NAN'
-train['installer'] = train['installer'].fillna('NAN')
-train.loc[train['installer'] == '0', 'installer'] = 'NAN'
-train['subvillage'] = train['subvillage'].fillna('NAN')
-train['scheme_name'] = train['scheme_name'].fillna('NAN')
-train['scheme_management'] = train['scheme_management'].fillna('NAN')
-
-train.loc[train['longitude'] == 0, 'latitude'] = -6.647724
-train.loc[train['longitude'] == 0, 'longitude'] = 35.022001
-
-
-headers = ['funder','installer','wpt_name','basin','subvillage','lga','ward','public_meeting','scheme_name','permit','scheme_management','extraction_type','extraction_type_group','extraction_type_class','management','management_group','payment','payment_type','water_quality','quality_group','quantity','quantity_group','source','source_type','source_class','waterpoint_type','waterpoint_type_group','status_group']
-
-for header in headers:
-    train[header] = lab_enc.fit_transform(train[header])
-
-
-#.astype(str)
-train['longitude'] = train['longitude'].astype(float)
-
-
-train = train.drop(["id","num_private","recorded_by", "date_recorded", "region"], axis = 1)
-
-
-#train.info()
-
-# %%
-# Replace date
 foo = np.array(train['construction_year'])
 avg = np.mean(foo[foo > 0])
 std = np.std(foo[foo > 0])
 count = len(foo[foo == 0])
 random_list = np.random.randint(avg - std, avg + std, size=count)
 train.loc[train['construction_year'] == 0,'construction_year'] = random_list
+
+
+specificHeaders = ['funder','installer','subvillage','scheme_name','scheme_management']
+for header in specificHeaders:
+    train[header] = train[header].fillna('NAN')
+
+
+train.loc[train['funder'] == '0', 'funder'] = 'NAN'
+train.loc[train['installer'] == '0', 'installer'] = 'NAN'
+train.loc[train['longitude'] == 0, 'latitude'] = -6.647724
+train.loc[train['longitude'] == 0, 'longitude'] = 35.022001
+
+#train['longitude'] = train['longitude'].astype(float)
+
+# %%
+#Process string data
+headers = ['funder','installer','wpt_name','basin','subvillage','lga','ward','public_meeting','scheme_name','permit','scheme_management','extraction_type','extraction_type_group','extraction_type_class','management','management_group','payment','payment_type','water_quality','quality_group','quantity','quantity_group','source','source_type','source_class','waterpoint_type','waterpoint_type_group','status_group']
+
+for header in headers:
+    train[header] = lab_enc.fit_transform(train[header])
+
+train = train.drop(["id","num_private","recorded_by", "date_recorded", "region"], axis = 1)
+
 
 #%%
 # Split data to train and test
