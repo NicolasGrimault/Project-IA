69cc84969d0729b67d9c0b0d6beb3ef22907fe24
Erwan ALLAIN <48276230+erwanallain@users.noreply.github.com>
2019-05-16 09:25:30 +0200

    Update mapping, refacto and scoring

--- python.py
+++ python.py
@@ -1,40 +1,60 @@
 # %%
 # Imports Data
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+lab_enc = preprocessing.LabelEncoder()
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 train = pd.read_csv('Data/TrainingSetValues.csv', header=0, sep = ",")
+#train.info()
 
 # %%
 # Display data
 train.info()
 
 # %%
-train['status_group'] = train['status_group'].map({'functional': 0, 'functional needs repair': 0.5, 'non functional': 1}).astype(float)
-train['source_class'] = train['source_class'].map({'groundwater': 0, 'surface': 1, 'unknown': 2}).astype(int)
-train['extraction_type'] = train['extraction_type'].map({ 'gravity': 0, 'submersible':1,'swn 80':2,'nira/tanira':3,'india mark ii':4,'other':5,'ksb':6,'mono':7,'windmill':8,'afridev':9,'other - rope pump':10,'india mark iii':11,'other - swn 81':12,'other - play pump':13,'cemo':14,'climax':15,'walimi':16,'other - mkulima/shinyanga':17}).astype(int)
-train['water_quality'] = train['water_quality'].map({'coloured': 0, 'fluoride': 1, 'fluoride abandoned': 2, 'milky': 3, 'salty': 4, 'salty abandoned': 5, 'soft': 6, 'unknown': 7}).astype(int)
-#train['quality_group'] = train['quality_group'].map({'good': 0, 'milky': 1, 'salty': 2, 'unknown': 3}).astype(int)
-train['quantity'] = train['quantity'].map({'dry': 0, 'enough': 1, 'insufficient': 2, 'seasonal': 3, 'unknown': 4}).astype(int)
-train['quantity_group'] = train['quantity_group'].map({'dry': 0, 'enough': 1, 'insufficient': 2, 'seasonal': 3, 'unknown': 4}).astype(int)
-train['source'] = train['source'].map({'dam': 0, 'hand dtw': 1, 'lake': 2, 'machine dbh': 3, 'other': 4, 'rainwater harvesting': 5, 'river': 6, 'shallow well': 7, 'spring': 8, 'unknown': 9}).astype(int)
-train['basin'] = train['basin'].map({'Internal': 0, 'Lake Nyasa': 1, 'Lake Rukwa': 2, 'Lake Tanganyika': 3, 'Lake Victoria': 4, 'Pangani': 5, 'Rufiji': 6, 'Ruvuma / Southern Coast': 7, 'Wami / Ruvu': 8}).astype(int)
-train['management'] = train['management'].map({'company': 0, 'other': 1, 'other - school': 2, 'parastatal': 3, 'private operator': 4, 'trust': 5, 'unknown': 6, 'vwc': 7, 'water authority': 8, 'water board': 9, 'wua': 10, 'wug': 11}).astype(int)
-train['payment'] = train['payment'].map({'never pay': 0, 'other': 1, 'pay annually': 2, 'pay monthly': 3, 'pay per bucket': 4, 'pay when scheme fails': 5, 'unknown': 6}).astype(int)
+train['funder'] = lab_enc.fit_transform(train['funder'].astype(str))
+train['installer'] = lab_enc.fit_transform(train['installer'].astype(str))
+train['wpt_name'] = lab_enc.fit_transform(train['wpt_name'].astype(str))
+train['basin'] = lab_enc.fit_transform(train['basin'].astype(str))
+train['subvillage'] = lab_enc.fit_transform(train['subvillage'].astype(str))
+train['lga'] = lab_enc.fit_transform(train['lga'].astype(str))
+train['ward'] = lab_enc.fit_transform(train['ward'].astype(str))
+train['public_meeting'] = lab_enc.fit_transform(train['public_meeting'].astype(str))
+train['scheme_name'] = lab_enc.fit_transform(train['scheme_name'].astype(str))
+train['permit'] = lab_enc.fit_transform(train['permit'].astype(str))
+train['scheme_management'] = lab_enc.fit_transform(train['scheme_management'].astype(str))
+train['extraction_type'] = lab_enc.fit_transform(train['extraction_type'].astype(str))
+train['extraction_type_group'] = lab_enc.fit_transform(train['extraction_type_group'].astype(str))
+train['extraction_type_class'] = lab_enc.fit_transform(train['extraction_type_class'].astype(str))
+train['management'] = lab_enc.fit_transform(train['management'].astype(str))
+train['management_group'] = lab_enc.fit_transform(train['management_group'].astype(str))
+train['payment'] = lab_enc.fit_transform(train['payment'].astype(str))
+train['payment_type'] = lab_enc.fit_transform(train['payment_type'].astype(str))
+train['water_quality'] = lab_enc.fit_transform(train['water_quality'].astype(str))
+train['quality_group'] = lab_enc.fit_transform(train['quality_group'].astype(str))
+train['quantity'] = lab_enc.fit_transform(train['quantity'].astype(str))
+train['quantity_group'] = lab_enc.fit_transform(train['quantity_group'].astype(str))
+train['source'] = lab_enc.fit_transform(train['source'].astype(str))
+train['source_type'] = lab_enc.fit_transform(train['source_type'].astype(str))
+train['source_class'] = lab_enc.fit_transform(train['source_class'].astype(str))
+train['waterpoint_type'] = lab_enc.fit_transform(train['waterpoint_type'].astype(str))
+train['waterpoint_type_group'] = lab_enc.fit_transform(train['waterpoint_type_group'].astype(str))
+train['status_group'] = lab_enc.fit_transform(train['status_group'].astype(str))
+
 train['longitude'] = train['longitude'].astype(float)
 
-from sklearn import preprocessing
-lab_enc = preprocessing.LabelEncoder()
+train = train.drop(["id","num_private","recorded_by", "date_recorded", "region"], axis = 1)
 
-train['funder'] = lab_enc.fit_transform(train['funder'].astype(str))
 
-# %%
-# Drop useless data
-train = train.drop(["id","num_private","recorded_by","funder","wpt_name","subvillage","region","lga","ward","public_meeting","scheme_management","scheme_name","permit","waterpoint_type","waterpoint_type_group","source_type","quality_group","payment_type","management_group","extraction_type_class","extraction_type_group","date_recorded"], axis = 1)
+train.info()
 
 # %%
 # Replace date
-import numpy as np
-
 foo = np.array(train['construction_year'])
 avg = np.mean(foo[foo > 0])
 std = np.std(foo[foo > 0])
@@ -44,18 +64,17 @@ train.loc[train['construction_year'] == 0,'construction_year'] = random_list
 
 #%%
 # Split data to train and test
-from sklearn.model_selection import train_test_split
-
 X_alltrain = train.values[:,:train.shape[1]-2]
 y_alltrain = train.values[:, train.shape[1]-1]
 X_train, X_test, y_train, y_test = train_test_split(X_alltrain, y_alltrain, random_state=42)
 
+
 # %%
 # Define model
-from sklearn.tree import DecisionTreeClassifier
-
-
-
-y_train_encoded = lab_enc.fit_transform(y_train)
 tree_clf = DecisionTreeClassifier(max_depth=4, random_state=42)
-tree_clf.fit(X_train,y_train_encoded)
+tree_clf.fit(X_train,y_train)
+
+y_pred = tree_clf.predict(X_test)
+score = accuracy_score(y_test, y_pred)
+
+print(score*100)
