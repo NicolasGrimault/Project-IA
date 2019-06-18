01404b98d7f45930e2d441fe36d3d907a435c909
Nicolas Grimault <nicolas.g21@live.fr>
2019-06-18 09:22:14 +0200

    Add ensemble model

--- python.py
+++ python.py
@@ -5,7 +5,10 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier,VotingClassifier
 from sklearn.metrics import accuracy_score
+from mlxtend.classifier import StackingClassifier
+from sklearn.linear_model import LogisticRegression
 
 lab_enc = preprocessing.LabelEncoder()
 
@@ -23,7 +26,8 @@ def generer_resultats(clf, data=Test.values, idx=Testindex):
     prediction = clf.predict(data)
     results = pd.DataFrame(prediction.astype(
         int), index=idx, columns=['status_group'])
-    results['status_group'] = lab_enc.inverse_transform(results['status_group'])
+    results['status_group'] = lab_enc.inverse_transform(
+        results['status_group'])
     results.to_csv('resultats%s.csv' % clf.__class__.__name__)
 
 # %%
@@ -61,7 +65,7 @@ for header in headers:
 Test = Test.drop(["id", "num_private", "recorded_by",
                         "date_recorded", "region"], axis=1)
 
-#As the label encoder is used later to deencode status_group, the last one used must be status_group
+# As the label encoder is used later to deencode status_group, the last one used must be status_group
 headers = ['funder', 'installer', 'wpt_name', 'basin', 'subvillage', 'lga', 'ward', 'public_meeting', 'scheme_name', 'permit', 'scheme_management', 'extraction_type', 'extraction_type_group', 'extraction_type_class',
            'management', 'management_group', 'payment', 'payment_type', 'water_quality', 'quality_group', 'quantity', 'quantity_group', 'source', 'source_type', 'source_class', 'waterpoint_type', 'waterpoint_type_group', "status_group"]
 for header in headers:
@@ -79,10 +83,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_alltrain, y_alltrain, random_state=42)
 
 
-
-
 # %%
-# Define model
+# Tree
 index_score = 0
 index_i = 0
 for i in range(10, 20):
@@ -96,7 +98,53 @@ for i in range(10, 20):
         index_i = i
         print(str(index_i) + ' : ' + str(index_score))
 
-
 finaltree_clf = DecisionTreeClassifier(max_depth=index_i, random_state=49)
-generer_resultats(finaltree_clf, Test)
+
+# %%
+# Forest
+forest_score = 0
+index_i = [0, 0, 0]
+for nbr_estimators in [50, 100, 150]:
+    for depth in range(4, 8):
+        for nbr_features in range(3, 5):
+            rnd_clf = RandomForestClassifier(
+                n_estimators=nbr_estimators, max_depth=depth, max_features=nbr_features, n_jobs=-1, random_state=42)
+            rnd_clf.fit(X_train, y_train)
+            y_pred = rnd_clf.predict(X_test)
+            score = accuracy_score(y_test, y_pred)*100
+            if score > forest_score:
+                forest_score = score
+                index_i = [nbr_estimators, depth, nbr_features]
+                print(str(nbr_estimators) + "-" + str(depth) + "-" +
+                      str(nbr_features) + ' : ' + str(forest_score))
+finalforest_clf = RandomForestClassifier(
+    n_estimators=index_i[0], max_depth=index_i[1], max_features=index_i[2], n_jobs=-1, random_state=42)
+
+# %% 
+#Prepare models
+for clf in (finaltree_clf, finalforest_clf):
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+# %%
+# Voting soft
+voting_soft_clf = VotingClassifier(estimators=[('tr', finaltree_clf),
+                                               ('rn', finalforest_clf)],
+                                   voting='soft')
+voting_soft_clf.fit(X_train, y_train)
+y_pred = voting_soft_clf.predict(X_test)
+print(str(100*accuracy_score(y_test, y_pred)))
+
+# %%
+# Stacking
+log_clf = LogisticRegression(random_state=42)
+Stacking_clf = StackingClassifier(classifiers=[
+    finaltree_clf,
+    finalforest_clf],
+    meta_classifier=log_clf)
+Stacking_clf.fit(X_train, y_train)
+y_pred = Stacking_clf.predict(X_test)
+print(str(100*accuracy_score(y_test, y_pred)))
+
+# generer_resultats(finalforest_clf, Test)
 print('END')
